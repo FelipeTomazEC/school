b5d789a38b5b4fe84e77ff2a8fa3a8bd1f583166
felipe.tomaz <felipe.tomaz@avenuecode.com>
2021-10-11 12:33:35 -0300

    feat: create student entity

---
+++ src/main/java/com/github/felipetomazec/school/Student.java
@@ -0,0 +1,51 @@
+package com.github.felipetomazec.school;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Student {
+    private String name;
+    private CPF cpf;
+    private Email email;
+    private Set<Phone> phones;
+
+    public Student (String name, CPF cpf, Email email) {
+        if(name == null) {
+            throw new IllegalArgumentException("A student must have a name.");
+        }
+
+        if(cpf == null) {
+            throw new IllegalArgumentException("A student must have a CPF.");
+        }
+
+        if(email == null) {
+            throw new IllegalArgumentException("A student must have an e-mail.");
+        }
+
+        this.cpf = cpf;
+        this.name = name.trim();
+        this.email = email;
+        this.phones = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public Set<Phone> getPhones() {
+        return phones;
+    }
+
+    public void addPhone(String ddd, String number) {
+        var phone = new Phone(ddd, number);
+        phones.add(phone);
+    }
+}
---
+++ src/main/java/com/github/felipetomazec/school/StudentBuilder.java
@@ -0,0 +1,29 @@
+package com.github.felipetomazec.school;
+
+public class StudentBuilder {
+    private String name;
+    private String cpf;
+    private String email;
+
+    public StudentBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public StudentBuilder setCpf(String cpf) {
+        this.cpf = cpf;
+        return this;
+    }
+
+    public StudentBuilder setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public Student build() {
+        var email = new Email(this.email);
+        var cpf = new CPF(this.cpf);
+
+        return new Student(name, cpf, email);
+    }
+}
